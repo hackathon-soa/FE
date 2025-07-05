df9e4f2e87935be8ebc75435297665593829278d
yangjiae12 <yangjiae12@gmail.com>
2025-07-06 02:43:10 +0900

    feat: 여행 코스 데이터 클래스 추가 #13

---
+++ app/src/main/java/com/example/soa/data/TravelCourse.kt
@@ -0,0 +1,11 @@
+package com.example.soa.data
+
+data class TravelCourse(
+    val title: String,
+    val location: String,
+    val schedule: String,
+    val gender: String,
+    val nickname: String,
+    val disability: String,
+    val profileImgRes: Int
+)
